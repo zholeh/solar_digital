c8b074503b6895a8e67be3b12891077d042de93f
Oleh Zhaivoronok <zholeg@ukr.net>
2018-11-28 16:06:17 +0200

    add transition to SelectedCities

--- src/components/WidgetSetting/SelectedCities.vue
+++ src/components/WidgetSetting/SelectedCities.vue
@@ -1,43 +1,43 @@
 <template>
   <div class="form-group">
     <draggable element="div" v-model="list" :options="dragOptions">
-
-      <div
-        v-for="widget in list"
-        :wd.prop="widget"
-        :key.prop="widget.id + widget.widgetOption.option"
-        :id="widget.id"
-      >
+      <transition-group type="transition" :name="'widget-list'">
         <div
-          id="CityButtonDell01"
-          style="
-            display: flex;
-            height: 50px;
-            width: 100%;
-            min-width: 375px;
-            background-color: #fff;
-            background-clip: border-box;
-            border: 1px solid rgba(0, 0, 0, 0.125);
-            border-radius: 0.25rem;
-          "
+          v-for="widget in list"
+          :key="widget.id + widget.widgetOption.option"
+          :id="widget.id"
         >
-          <div style="height: 100%;
-            width: 10px;
-            background-color: brown;"></div>
-          <span></span>
           <div
-            v-on:click="delCity(widget.id, widget.widgetOption.option)"
-            style="font-size: x-large; margin: auto 5px auto 5px; cursor: pointer;"
-          >✖</div>
-          <span style="margin: auto 5px auto 5px;">
-            <span class="badge badge-info m-1">{{widget.widgetOption.option}}</span>
-            <img
-              :src="`http://openweathermap.org/images/flags/${widget.country.toLowerCase()}.png`"
-            >
-            {{widget.name}}, ID: {{widget.id}}, {{widget.country}}
-          </span>
+            id="CityButtonDell01"
+            style="
+              display: flex;
+              height: 50px;
+              width: 100%;
+              min-width: 375px;
+              background-color: #fff;
+              background-clip: border-box;
+              border: 1px solid rgba(0, 0, 0, 0.125);
+              border-radius: 0.25rem;
+            "
+          >
+            <div style="height: 100%;
+              width: 10px;
+              background-color: brown;"></div>
+            <span></span>
+            <div
+              v-on:click="delCity(widget.id, widget.widgetOption.option)"
+              style="font-size: x-large; margin: auto 5px auto 5px; cursor: pointer;"
+            >✖</div>
+            <span style="margin: auto 5px auto 5px;">
+              <span class="badge badge-info m-1">{{widget.widgetOption.option}}</span>
+              <img
+                :src="`http://openweathermap.org/images/flags/${widget.country.toLowerCase()}.png`"
+              >
+              {{widget.name}}, ID: {{widget.id}}, {{widget.country}}
+            </span>
+          </div>
         </div>
-      </div>
+      </transition-group>
     </draggable>
   </div>
 </template>
@@ -46,6 +46,10 @@
 #WidgetContainer:hover #CityButtonDell01 {
   opacity: 1 !important;
   font-size: x-large !important;
+}
+
+.widget-list-move {
+  transition: transform 0.5s;
 }
 </style>
 
